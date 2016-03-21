074327d36c6ceedd38d22d57c7486aecbcb10e7b
macLovesOlives <ostler.mackenzie@gmail.com>
2016-03-20 20:59:11 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var emoji = require('node-emoji');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Welcome to twitSearch',
+    emoji: emoji
 
   });
 });
